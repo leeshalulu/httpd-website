c77bd02231861f51352f1533ff129cffc01ac55b
leeshalulu <leeshalulu01@gmail.com>
2026-06-06 12:57:17 +0530

    add a script file for host web content

---
+++ httpd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#install and configure httpd webserver
+sudo dnf install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+#host simple web page
+
+echo "welcome to synnefo"| sudo tee /var/www/html/index.html
